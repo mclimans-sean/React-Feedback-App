782b2a49ed7d945f5a75b4621f4e3824d54ade98
SEAN MCLIMANS <seanmclimans@gmail.com>
2017-12-30 18:55:19 -0700

    Start form validation

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -2,6 +2,7 @@ import _ from 'lodash';
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
 import SurveyField from './SurveyField';
+import { Link } from 'react-router-dom';
 
 const FIELDS = [
   { label: 'Survey Title', name: 'title' },
@@ -30,8 +31,12 @@ class SurveyForm extends Component {
       <div>
         <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
           {this.renderFields()}
-          <button className="btn waves-effect waves-light" type="submit">
-            Submit
+          <Link to="/surveys" className="btn red waves-light">
+            Cancel
+          </Link>
+          <button className="btn waves-effect right waves-light" type="submit">
+            Next
+            <i className="material-icons right">done</i>
           </button>
         </form>
       </div>
@@ -39,6 +44,17 @@ class SurveyForm extends Component {
   }
 }
 
+function validate(values) {
+  const errors = {};
+
+  if (!values.title) {
+    errors.title = 'You must provide a title';
+  }
+
+  return errors;
+}
+
 export default reduxForm({
+  validate,
   form: 'surveyForm'
 })(SurveyForm);
